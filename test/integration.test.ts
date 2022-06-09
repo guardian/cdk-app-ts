baf2df3aba3b2ec5b4e1034e1eadb40f2b75f567
Nicolas Long <nicolas.long@theguardian.com>
2022-06-09 10:34:30 +0100

    Default skip integration test for now

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,10 +1,13 @@
 import { execSync } from 'child_process';
 import { GuCDKTypescriptProject } from '../src';
 
-test('synth (for inspection only)', () => {
+test.skip('synth (for inspection only)', () => {
   const outdir = 'tmp';
   execSync(`rm -rf ${outdir}`);
 
   const prj = new GuCDKTypescriptProject({ name: 'test', outdir });
   prj.synth();
+
+  // TODO run e.g. test and lint on the output and check exit codes. Then delete
+  // the directory again.
 });
